371b3f21a4bfe5ce09543f2ea3b839dabcbc9fdc
Jake Grigsby <jcg6dn@virginia.edu>
2020-03-18 17:57:12 -0400

    batch norm in actor and critic

--- deep_control/nets.py
+++ deep_control/nets.py
@@ -7,14 +7,17 @@ class BaselineActor(nn.Module):
     def __init__(self, obs_size, action_size):
         super().__init__()
         self.fc1 = nn.Linear(obs_size, 256)
+        self.bn1 = nn.BatchNorm1d(256)
         self.fc2 = nn.Linear(256, 400)
+        self.bn2 = nn.BatchNorm1d(400)
         self.fc3 = nn.Linear(400, 128)
+        self.bn3 = nn.BatchNorm1d(128)
         self.out = nn.Linear(128, action_size)
     
     def forward(self, state):
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
+        x = F.relu(self.bn1(self.fc1(state)))
+        x = F.relu(self.bn2(self.fc2(x)))
+        x = F.relu(self.bn3(self.fc3(x)))
         act = torch.tanh(self.out(x))
         return act
 
@@ -22,15 +25,18 @@ class BaselineCritic(nn.Module):
     def __init__(self, obs_size, action_size):
         super().__init__()
         self.fc1 = nn.Linear(obs_size, 400)
+        self.bn1 = nn.BatchNorm1d(400)
         self.fc2 = nn.Linear(400 + action_size, 300)
+        self.bn2 = nn.BatchNorm1d(300)
         self.fc3 = nn.Linear(300, 128)
+        self.bn3 = nn.BatchNorm1d(128)
         self.out = nn.Linear(128, 1)
     
     def forward(self, state, action):
-        x = F.relu(self.fc1(state))
+        x = F.relu(self.bn1(self.fc1(state)))
         x_act = torch.cat((x, action), dim=1)
-        x = F.relu(self.fc2(x_act))
-        x = F.relu(self.fc3(x))
+        x = F.relu(self.bn2(self.fc2(x_act)))
+        x = F.relu(self.bn3(self.fc3(x)))
         val = self.out(x)
         return val
 
