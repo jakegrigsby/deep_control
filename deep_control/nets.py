7334938f4287e72ee46310d3f944133489ccb032
jakegrigsby <jcg6dn@virginia.edu>
2020-08-26 13:07:45 -0400

    simpler BaselinePixelEncoder

--- deep_control/nets.py
+++ deep_control/nets.py
@@ -85,30 +85,24 @@ class BaselinePixelEncoder(nn.Module):
     def __init__(self, obs_shape):
         super().__init__()
         channels = obs_shape[0]
-        self.conv1 = nn.Conv2d(channels, 32, kernel_size=8, stride=4)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.conv1 = nn.Conv2d(channels, 32, kernel_size=3, stride=2)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, stride=1)
 
         output_height, output_width = utils.compute_conv_output(
-            utils.compute_conv_output(
-                utils.compute_conv_output(
-                    obs_shape[1:], kernel_size=(8, 8), stride=(4, 4)
-                ),
-                kernel_size=(4, 4),
-                stride=(2, 2),
-            ),
+            utils.compute_conv_output(obs_shape[1:], kernel_size=(3, 3), stride=(2, 2)),
             kernel_size=(3, 3),
             stride=(1, 1),
         )
 
-        self.fc = nn.Linear(output_height * output_width * 64, 400)
+        self.fc = nn.Linear(output_height * output_width * 32, 400)
+        self.ln = nn.LayerNorm(400)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc(x))
+        x = self.ln(x)
         return x
 
 
